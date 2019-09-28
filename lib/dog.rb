7a98ab939eb8788ccd71985b26a3ffc4e01d35cf
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-28 16:21:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,4 +23,63 @@ class Dog
 end
 
 
+class Dog
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
+end
+
+# PERSON AND LIB/PERSON.RB
+
+class Person
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
+end
+
+# Notes and practice 
+
+class Record
+  
+  def genre=(records_genre)
+    @grenre = records_genre
+  end 
+  
+  def genre
+    @genre 
+  end 
+  
+end 
+
+beasty_boys = Record.new
+beasty_boys.genre = "Rap and R&B"
+beasty_boys.gerne 
+
+
   
